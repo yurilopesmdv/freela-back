e5391d00dc1527728626f2aab3f13fddc062b5b0
yurilopesm <yurilopesm@hotmail.com>
2023-05-28 21:20:33 -0300

    fix: solving deploy problems

--- src/app.js
+++ src/app.js
@@ -10,4 +10,5 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
+const port = process.env.PORT || 5000
 app.listen(process.env.PORT, () => console.log(`Server is running on ${process.env.PORT} port`))
